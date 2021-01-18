7b641c8a1329c92240b4a05b7d83c17005a3bb00
mrzhangcc <cczhang1987@hotmail.com>
2021-01-18 11:26:29 -0500

    Update

--- src/api/env.js
+++ src/api/env.js
@@ -8,8 +8,10 @@ if (process.env.NODE_ENV === 'development') {
   host = 'http://localhost:8080'
   baseUrl = 'http://localhost/jiuqian-mobile'
 } else if (process.env.NODE_ENV === 'production') {
-  host = 'https://www.365huijia.com'
-  baseUrl = 'https://www.365huijia.com'
+  // host = 'https://www.365huijia.com'
+  // baseUrl = 'https://www.365huijia.com'
+    host = 'https://jqdz.online'
+    baseUrl = 'https://jqdz.online'
 }
 
 export {
